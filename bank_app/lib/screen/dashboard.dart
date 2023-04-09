2f2e272eb7e777c63c48f6efc9ec659bffba8f3b
Mateusz Glejzer <mateusz.glejzer@fortedigital.no>
2023-04-09 19:41:33 +0200

    create structure files

---
+++ bank_app/lib/screen/dashboard.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
